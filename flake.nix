8bd0dae3cdbe51c685483396074dd7c12cc4a7e7
bryce <bryce.hoehn@icloud.com>
2026-05-08 21:05:33 -0400

    migrate to nix2container with base image as flake input

--- flake.nix
+++ flake.nix
@@ -4,37 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix2container.url = "github:nlewo/nix2container";
+    base.url = "github:podmania/base";
   };
 
-  outputs = { self, nixpkgs, nix2container }: let
+  outputs = { self, nixpkgs, nix2container, base }: let
     system = builtins.currentSystem;
     pkgs = nixpkgs.legacyPackages.${system};
     n2c = nix2container.outputs.packages.${system}.nix2container;
-
-    distrolessImage = {
-      imageName = "ghcr.io/podmania/base";
-      imageDigest = "sha256:488c95da5023d19b22b342826d5ea1f3638ffe7642e58e9fc2fa8f4e4ff99289";
-      sha256 = {
-        x86_64-linux = "sha256-0pPkbjbKS+u9IJ3Gj76OxhHZN4NtPa4rJgFrZhptgVA=";
-        aarch64-linux = "sha256-sto1YwQHJdVPAJMWfE8yyzsGy6JVFT4csr6zz3Ma12c=";
-      };
-    };
   in {
     packages.${system} = {
       qbittorrent-nox-image = n2c.buildImage {
         name = "qbittorrent-nox";
         tag = "latest";
-
-        fromImage = n2c.pullImage {
-          imageName = distrolessImage.imageName;
-          imageDigest = distrolessImage.imageDigest;
-          sha256 = distrolessImage.sha256.${system};
-        };
-
-        contents = [
-          pkgs.qbittorrent-nox
-        ];
-
+        fromImage = base.packages.${system}.base-image;
+        copyToRoot = [ pkgs.qbittorrent-nox ];
         config = {
           ExposedPorts = {
             "8080/tcp" = {};
@@ -56,7 +39,7 @@
 
       default = self.packages.${system}.qbittorrent-nox-image;
     };
-    
+
     qbittorrent-noxVersion = pkgs.qbittorrent-nox.version;
   };
 }
